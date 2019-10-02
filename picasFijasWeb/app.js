0e3bf43d61448877ed01b12017d654cc8a2e4322
daniel <hdanielmm@gmail.com>
2019-10-02 05:14:36 +0200

    add files from remote

---
+++ picasFijasWeb/app.js
@@ -0,0 +1,5 @@
+var source   = document.getElementById("entry-template").innerHTML;
+var template = Handlebars.compile(source);
+
+var context = {title: "My New Post", body: "This is my first post!"};
+var html    = template(context);
